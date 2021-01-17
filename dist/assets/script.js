3a4776ec2ecffd7be8a0f577df0f5401b76ed50a
Damarowen <67860422+Damarowen@users.noreply.github.com>
2021-01-18 01:21:18 +0700

    update typeit

---
+++ dist/assets/script.js
@@ -0,0 +1,26 @@
+
+
+  
+  new TypeIt("#formElement1", {
+    strings: "Damar Oen I",
+    cursor: true	
+  }).go();
+  
+  
+  new TypeIt("#formElement2", {
+      strings: "A Worker, Hard Worker",
+      startDelay: 1500,
+      cursor: true	
+    }).go();
+    
+    new TypeIt("#formElement3", {
+      strings: "Learn Something New",
+      startDelay: 4000,
+      cursor: true	
+    }).go();
+  
+  
+    new TypeIt("#formElement4", {
+      strings: "Let's work together..",
+      waitUntilVisible: true
+    }).go();
